edad07a6b404efab4afad02c7728c4dd6875b7c3
maxfe <maxfe@IFENNA>
2024-01-24 23:06:32 +0100

    Add project files.

---
+++ january 2024/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace january_2024
+{
+    internal class Circle
+    {
+        public static void Radius()
+        {
+            Console.WriteLine("the radius is 45");
+
+        }
+    }
+}
---
+++ january 2024/Program.cs
@@ -0,0 +1,17 @@
+﻿using january_2024;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("ifenna nah boss");
+        welcomememo();
+        Circle.Radius();
+    }
+
+    static void welcomememo()
+    {
+        Console.WriteLine("welcome to the cohort class, 4pm daily");
+    }
+}
