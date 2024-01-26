73de69112d80627083447c3bebe87bf7281694e8
ifenna ezeh <maxfezzy@gmail.com>
2024-01-26 21:35:13 +0100

    datatypes and arithmetic operators

--- january 2024/Program.cs
+++ january 2024/Program.cs
@@ -1,17 +1,61 @@
 ﻿using january_2024;
+using Microsoft.VisualBasic;
 
 internal class Program
+
 {
+    //types of datatypes
+    //integer int
+    //string
+    //float
+    //bool boolean
+    //decimal
+    //double
+    //long
+    //byte
+
+    static int v = 56;
+    static string firstname = "Ifenna";
+    static string lastname = "Ezeh";
+    float x = 6754657;
+    bool y = false;
+    decimal values = 899.7879m;
+    double numerical = 755676.89678;
+    static  DateTime dateonly = DateTime.Now;
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("ifenna nah boss");
-        welcomememo();
-        Circle.Radius();
-    }
+        //Console.WriteLine(v);
 
+        // Console.WriteLine("Hello"+" "+lastname+" "+firstname);
+
+        //Console.WriteLine(dateonly);
+
+        // const int data = 6464;
+
+        //Console.WriteLine(data);
+       // welcomememo();
+
+
+    }
+    //conversion
+    //implicit casting
+    //explicit casting
     static void welcomememo()
     {
-        Console.WriteLine("welcome to the cohort class, 4pm daily");
+        //implicit = increasing
+        int data1 = 78;
+        double data2 = data1;
+        Console.WriteLine(data2);
+
+        //explicit = decreasing a datatype
+        double v = 456.22;
+        int v2 = (int)v;
+        Console.WriteLine(v2);
+
+      
     }
+
+    
+
 }
