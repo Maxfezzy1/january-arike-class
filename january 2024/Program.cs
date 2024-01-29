ff18161da8bb8ebe40a72c7170d2b1f613806ab9
ifenna ezeh <maxfezzy@gmail.com>
2024-01-29 14:34:24 +0100

    changes do while while for loop foreach

--- january 2024/Program.cs
+++ january 2024/Program.cs
@@ -4,16 +4,7 @@ using System.Reflection.Emit;
 
 internal class Program
 {
-    // assignment operators =, +=, -=, *=, /=, &=, %=
-    // logical operators &&, ||, ! 
-    // comparison operators ==, !=, >, <, >=, <=
-    // statement;conditional statement & looping
-    // conditional statement ; if&else-statement, switch case
-    //   || this means OR
-    //   != this means NOT EQUAL TO
-    //   == this means 
-    //   && this means AND
-    // looping
+    //looping
     //while
     //dowhile
     //for
@@ -21,120 +12,40 @@ internal class Program
 
     private static void Main(string[] args)
     {
-        int y = 25;
-        y += 23;
-        y -= 10;
-        y *= 10;
-        y /= 2;
-        //Console.WriteLine(y);
-        //ConditionalStatement();
-        //CombinedMethod();
-        //CombinedMethod2();
-        SwitchCaseMethod();
-    }
-    static void ConditionalStatement()
-    {
-        int x = 245;     int b = 90;
-        int v = 654;     int c = 76;
-
-        if(x == v)
+        //how to declare more values to a variable... use of an array[]
+        //foreach-loop
+        int[] numbers = { 1, 2, 3, 89, 4, 5, 6, 7, 8, 9, 98};
+        foreach (int i in numbers)
         {
-            Console.WriteLine("the result is correct");
-        }
-        else
-        {
-            Console.WriteLine("the result is not correct");
+            Console.WriteLine(i);
         }
 
-        if (b != v)
+        //Forloop
+        for (int i = 0; i <= 10; i++)
         {
-            Console.WriteLine("the result is correct");
+           Console.WriteLine(i);
         }
-        else
-        {
-            Console.WriteLine("the result is not correct");
-        }
-        if (b > 100)
-        {
-            Console.WriteLine("B is greater");
-        }
-        else
-        {
-            Console.WriteLine("B is not greater");
-        }
-    }
-    static void CombinedMethod()
-    {
-        Console.WriteLine("Hello what would you like to get");
-        Console.WriteLine("select 1. for coffee, 2. for bread, 3. for tea");
-        int result = int.Parse(Console.ReadLine());
-        // Parse is used for converting string datatype to int datatype
 
-        int x = 1;
-        int b = 2;
-        int v = 3;
+        //do while
+        int y = 0;
+        do
+        {
+            Console.WriteLine("do while result is "+" "+y);
+            y++;
+        }
+        while (y < 20);
 
-        if(result == x || result == b || result == v)
+        //while
+        int x = 0;
+        while (x < 20)
         {
-            Console.WriteLine("thank you for your patronage");
+            Console.WriteLine("while result is"+" "+x);
+            x++;
         }
-        else
-        {
-            Console.WriteLine("Please select the right value");
-        }
-    }
-    static void CombinedMethod2()
-    {
-        Console.WriteLine("Hello what would you like to get");
-        Console.WriteLine("select 2. for bread");
-        int result = int.Parse(Console.ReadLine());
-        Console.WriteLine("please input 3 for your budget price");
-        int result2 = int.Parse(Console.ReadLine());
 
-        int b = 2;
-        int v = 3;
-        if (result == b && result2 == v)
-        {
-            Console.WriteLine("thank you for your patronage");
-        }
-        else
-        {
-            Console.WriteLine("please select the right value");
-        }
+
     }
-    static void SwitchCaseMethod()
-    {
-        Label:
-        Console.WriteLine("please select days of the week in Numerical Format");
-        int result = int.Parse(Console.ReadLine());
-        switch(result)
-        {
-            case 1:
-                Console.WriteLine("Monday");
-                break;
-            case 2:
-                Console.WriteLine("Tuesday");
-                break;
-            case 3:
-                Console.WriteLine("Wednesday");
-                break;
-            case 4:
-                Console.WriteLine("Thursday");
-                break;
-            case 5:
-                Console.WriteLine("Friday");
-                break;
-            case 6:
-                Console.WriteLine("Saturday");
-                break;
-            case 7:
-                Console.WriteLine("Sunday");
-                break;
-                default:
-                Console.WriteLine("the value does not exist");
-                goto Label;
-        }
-    }
+   
 
 
 
