309cc9d587517aff4f017e5b3489ff90b34e1aae
ifenna ezeh <maxfezzy@gmail.com>
2024-02-01 22:57:54 +0100

    fixes

--- january 2024/Program.cs
+++ january 2024/Program.cs
@@ -4,52 +4,25 @@ using System.Reflection.Emit;
 
 internal class Program
 {
-    //looping
-    //while
-    //dowhile
-    //for
-    //foreach
+    int v = 78; //this is called a field
+    int age { get; set; } = 67;//properties ; used when u want to send something to your database
 
-    private static void Main(string[] args)
+    private static void Main(string[] args)//method
     {
-        //how to declare more values to a variable... use of an array[]
-        //foreach-loop
-        int[] numbers = { 1, 2, 3, 89, 4, 5, 6, 7, 8, 9, 98};
-        foreach (int i in numbers)
-        {
-            Console.WriteLine(i);
-        }
+        User.UserInfo();
+        User.Age = 88;
 
-        //Forloop
-        for (int i = 0; i <= 10; i++)
-        {
-           Console.WriteLine(i);
-        }
+        Student.StudentInfo();
+        
 
-        //do while
-        int y = 0;
-        do
-        {
-            Console.WriteLine("do while result is "+" "+y);
-            y++;
-        }
-        while (y < 20);
+        //instatiation; used to call out the nonstatic in another class
+        //the object known as stud can change into any name of your choice
 
-        //while
-        int x = 0;
-        while (x < 20)
-        {
-            Console.WriteLine("while result is"+" "+x);
-            x++;
-        }
+        Student stud = new Student(101, "grace",22 , "chemistry");
+        stud.StudentDetails();
+        
 
 
     }
    
-
-
-
-
-
-
 }
---
+++ january 2024/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace january_2024
+{
+    //access modifiers
+    //types of access modifiers
+    //public
+    //private
+    //internal
+    //protected
+
+    //nonstatic; a non-static class accomodates both a static and non static.
+    public class Student
+    {
+        int Id;
+        String Name;
+        int Age;
+        String Department;
+
+        public Student(int id, string name, int age, string dept)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Department = dept;
+
+        }
+
+        public void StudentDetails()
+        {
+            Console.WriteLine("his name is {0} from {1} Class,i'm {2}", Name, Department, Age);
+        }
+
+        public static void StudentInfo()
+        {
+            Console.WriteLine("his name is ude from math class");
+        }
+    }
+}
