b009f524255f47924778926bec68800128791c06
ifenna ezeh <maxfezzy@gmail.com>
2024-02-02 09:04:17 +0100

    escape-sequence

--- january 2024/Program.cs
+++ january 2024/Program.cs
@@ -1,27 +1,26 @@
-﻿using january_2024;
+﻿
 using Microsoft.VisualBasic;
 using System.Reflection.Emit;
 
 internal class Program
 {
-    int v = 78; //this is called a field
-    int age { get; set; } = 67;//properties ; used when u want to send something to your database
-
-    private static void Main(string[] args)//method
+    //escape sequence
+    // \'
+    // \"
+    // \n = new line before the next word
+    // \r
+    // \t
+    // \0
+    // \b
+  
+    private static void Main(string[] args)
     {
-        User.UserInfo();
-        User.Age = 88;
-
-        Student.StudentInfo();
-        
-
-        //instatiation; used to call out the nonstatic in another class
-        //the object known as stud can change into any name of your choice
-
-        Student stud = new Student(101, "grace",22 , "chemistry");
-        stud.StudentDetails();
-        
-
+        string sample = "Hello\nVictor";
+        string sample2 = "c:\\codebuns\\file.cs";
+        string sample3 = "i am a \"junior developer\".";
+        Console.WriteLine(sample);
+        Console.WriteLine(sample2);
+        Console.WriteLine(sample3);
 
     }
    
