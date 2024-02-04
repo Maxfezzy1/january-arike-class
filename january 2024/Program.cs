1bfe62a99c6b878e4f820253bf41c75d0d448533
ifenna ezeh <maxfezzy@gmail.com>
2024-02-04 21:14:16 +0100

    generics

--- january 2024/Program.cs
+++ january 2024/Program.cs
@@ -4,35 +4,17 @@ using System.Reflection.Emit;
 
 internal class Program
 {
-    int v = 78; //this is called a field
-    int age { get; set; } = 67;//properties ; used when u want to send something to your database
 
     private static void Main(string[] args)//method
     {
-        User.UserInfo();
-        User.Age = 88;
+        Student.Ifenna<int>(78, 89);
+        Student.Ifenna<string>("hello", "welcome");
 
-        Student.StudentInfo();
-        
 
-        //instatiation; used to call out the nonstatic in another class
-        //the object known as stud can change into any name of your choice
-
-        Student stud = new Student(101, "grace",22 , "chemistry");
-        stud.StudentDetails();
-
-        Student.Radius();//this student class was called but the methods in the circle class can be used because the student class inherited from the circle class
 
     }
 
-    public class user
-    {
-        public void MyMethod()
-        {
-
-
-        }
-    }
+  
 
    
 }
--- january 2024/Student.cs
+++ january 2024/Student.cs
@@ -6,54 +6,15 @@ using System.Threading.Tasks;
 
 namespace january_2024
 {
-   public enum DaysofTheWeek
+   //generic means something that is not specific to a particular datatype in C# until u declare them urself
+   //generic <T> put this in front and it makes either the method or class generic
+    public class Student
     {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
-    }
-
-    public enum Gender
-    {
-        Male,
-        Female,
-        Unknown,
-        Others
-
-    }
-
-    public class Student : Circle // base class while the student class is called derived class
-    {
-        public Gender Gender {  get; set; }
-
-
-        int Id;
-        String Name;
-        int Age;
-        String Department;
-
-        public Student(int id, string name, int age, string dept)
+        public static void Ifenna<T>(T value1, T value2)
         {
-            Id = id;
-            Name = name;
-            Age = age;
-            Department = dept;
+            Console.WriteLine("value 1 = {0} and Value2 = {1}", value1, value2);
 
         }
-
-        public void StudentDetails()
-        {
-            Console.WriteLine("his name is {0} from {1} Class,i'm {2}", Name, Department, Age);
-        }
-
-        public static void StudentInfo()
-        {
-            Console.WriteLine(Gender.Male);
-            Console.WriteLine("his name is ude from math class");
-        }
+      
     }
 }
