fbad589209651bbe7e9843bc6849e6b06e5344f0
ifenna ezeh <maxfezzy@gmail.com>
2024-03-01 11:59:53 +0100

    dictionary

--- january 2024/Program.cs
+++ january 2024/Program.cs
@@ -9,73 +9,42 @@ internal class Program
 
    private static void Main(string[] args)
    {
-        //List
-        List <int> Numbers = new List <int> ();
-        Numbers.Add (17);
-        Numbers.Add(18);
-        Numbers.Add (19);
-        Numbers.Add(34);
-        Numbers.Add(65);
+        CarModule CarList1 = new CarModule() { CarId = 201, Name = "Honda", Price = 2000000, Model = 2002 };
+        CarModule CarList2 = new CarModule() { CarId = 202, Name = "Suv", Price = 2500000, Model = 2005 };
+        CarModule CarList3 = new CarModule() { CarId = 203, Name = "Camry", Price = 3200000, Model = 2012 };
+        CarModule CarList4 = new CarModule() { CarId = 204, Name = "Toyota", Price = 1000000, Model = 2045 };
+        CarModule CarList5 = new CarModule() { CarId = 205, Name = "Tesla", Price = 7420000, Model = 2022 };
+        CarModule CarList6 = new CarModule() { CarId = 206, Name = "Bugatti", Price = 1200000, Model = 2010 };
 
-        foreach (int i in Numbers)
+        Dictionary<int,CarModule> Cardictionary = new Dictionary<int,CarModule>();
+        Cardictionary.Add(1, CarList1 );
+        Cardictionary.Add(CarList2.CarId, CarList2);
+        Cardictionary.Add(CarList3.CarId, CarList3);
+        Cardictionary.Add(CarList4.CarId, CarList4);
+        Cardictionary.Add(CarList5.CarId, CarList5);
+        Cardictionary.Add(CarList6.CarId, CarList6);
+
+        foreach (var item in Cardictionary)
         {
-            Console.WriteLine (  i   );
+            Console.WriteLine("Key"+" "+ item.Key);
+           var car = item.Value;
+            Console.WriteLine("Name = {0}, Price = {1}, Model = {2}",car.Name,car.Price, car.Model);
         }
 
 
-        //Queue
-        Queue <string> words = new Queue <string> ();
-        words.Enqueue("chalk");
-        words.Enqueue("Board");
-        words.Enqueue("Purse");
-        words.Enqueue("Bag");
-        words.Enqueue("Chair");
-        words.Enqueue("Table");
-
-        Console.WriteLine("Total Elements : {0}", words.Count());//this simply counts the total elements
-
-        var Data = words.Dequeue ();//this prints and remove the first elements
-        Console.WriteLine (Data);
-
-        Console.WriteLine("Total Elements : {0}", words.Count());
-
-        if(words.Count() > 0)
-        {
-            Console.WriteLine (words.Peek ());//this prints and do not remove the first element
-        }
-        Console.WriteLine("Total Elements : {0}", words.Count());
-
-        var data = words.Dequeue ();
-        Console.WriteLine (data);
-
-        Console.WriteLine("Total Elements : {0}", words.Count());
 
 
 
-        //Stacks
-        Stack<int> stacklist = new Stack<int> ();
-        stacklist.Push (1);
-        stacklist.Push (2);
-        stacklist.Push (3);
-        stacklist.Push (4);
-        stacklist.Push (5);
-        stacklist.Push (6);
-        stacklist.Push (7);
 
-        Console.WriteLine("Numbers of elements in stack :{0}", stacklist.Count());
 
-        while (stacklist.Count > 0)
-        {
-            Console.Write(stacklist.Pop() + " , ");
-        }
-        Console.WriteLine("Numbers of elements in stack :{0}", stacklist.Count());
 
-    } 
 
-      
-     
+    }
 
-    
+
+
+
+
 
 
 }
